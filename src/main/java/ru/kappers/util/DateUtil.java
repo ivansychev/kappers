36252a2df80692ecc3f5cc739cab3d038e3988b2
Samson Boroda <samborod76@gmail.com>
2018-10-19 03:19:09 -0700

    DateUtil

---
+++ src/main/java/ru/kappers/util/DateUtil.java
@@ -0,0 +1,27 @@
+package ru.kappers.util;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+	private static DateFormat df=new SimpleDateFormat("yyyMMdd");
+	
+	public static Timestamp getCurrentTime() {
+		return new Timestamp(System.currentTimeMillis());
+	}
+	
+	public static Timestamp convertDate(String date) {
+		try {
+			Date ddate=df.parse(date);
+			return new Timestamp(ddate.getTime());
+		} catch (ParseException e) {
+			System.out.println("WRONG DATA GIVEN");
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+}
