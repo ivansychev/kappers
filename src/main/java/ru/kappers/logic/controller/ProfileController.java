64cbbb0c51dc3e7b8742dd6789fe8db9e599646f
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-04-29 23:40:28 +0300

    Рефакторинг. В ProfileController удалены лишние импорты

--- src/main/java/ru/kappers/logic/controller/ProfileController.java
+++ src/main/java/ru/kappers/logic/controller/ProfileController.java
@@ -1,25 +1,13 @@
 package ru.kappers.logic.controller;
 
-import com.google.gson.Gson;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import lombok.extern.log4j.Log4j;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.SecurityContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ru.kappers.model.Fixture;
 import ru.kappers.model.User;
-import ru.kappers.service.FixtureService;
 import ru.kappers.service.RolesService;
 import ru.kappers.service.UserService;
-import ru.kappers.util.DateUtil;
-import ru.kappers.util.JsonUtil;
-
-import java.time.LocalDate;
-import java.util.Map;
 
 @Log4j
 @RestController
