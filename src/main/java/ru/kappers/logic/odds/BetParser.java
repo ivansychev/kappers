f7fa56d13730e89bdab1bb646b0dbf83c0ac5c2a
soufee <soufee@mail.ru>
2019-05-04 15:14:14 +0300

    С линка турнира получаем линки каждого события

--- src/main/java/ru/kappers/logic/odds/BetParser.java
+++ src/main/java/ru/kappers/logic/odds/BetParser.java
@@ -4,7 +4,7 @@ import java.net.MalformedURLException;
 import java.util.List;
 
 public interface BetParser<T> {
-    List<String> loadUrlsOfEvents(String url) throws MalformedURLException;
-    List<T> loadEventsOfTournament(String url);
+    List<String> loadEventUrlsOfTournament(String url);
+    T loadEventOdds(String url);
 
 }
--- src/main/java/ru/kappers/logic/odds/LeonBetParser.java
+++ src/main/java/ru/kappers/logic/odds/LeonBetParser.java
@@ -1,13 +1,9 @@
 package ru.kappers.logic.odds;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import jdk.nashorn.internal.parser.JSONParser;
+import com.google.gson.*;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
-import ru.kappers.model.dto.LeonOddsDTO;
+import ru.kappers.exceptions.BetParserException;
+import ru.kappers.model.dto.leon.LeonOddsDTO;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,15 +17,37 @@ import java.util.List;
 public class LeonBetParser implements BetParser<LeonOddsDTO> {
     private static final String LEON_ADDRESS = "https://www.leon.ru";
 
+    /**
+     * Метод возвращает список урлов доступных спортивных событий конкретного турнира
+     *
+     * @param url - линк веб страницы турнира, из котогрого нужно получить список событий
+     */
     @Override
-    public List<String> loadUrlsOfEvents(String url) {
+    public List<String> loadEventUrlsOfTournament(String url) {
+        log.info("loadEventUrlsOfTournament {}", url);
         List<String> urlsOfEvents = new ArrayList<>();
+        JsonArray arr = getArrayOfEventsByURL(url);
+        for (int i = 0; i < arr.size(); i++) {
+            JsonElement el = arr.get(i);
+            String eventLink = el.getAsJsonObject().get("url").getAsString();
+            urlsOfEvents.add(eventLink);
+        }
+        return urlsOfEvents;
+    }
+
+    /**
+     * Метод возвращает массив объектов JSON, полученных в результате парсинга веб страницы указанного урла
+     *
+     * @param url - линк веб страницы, которую нужно распарсить
+     */
+    private JsonArray getArrayOfEventsByURL(String url) {
 
         URL address = null;
         try {
             address = new URL(LEON_ADDRESS + url);
         } catch (MalformedURLException e) {
-            throw new RuntimeException("Не удалось сформировать URL", e);
+            log.error("Не удалось сформировать URL", e);
+            throw new BetParserException("Не удалось сформировать URL", e);
         }
         StringBuilder sb = new StringBuilder();
 
@@ -41,6 +59,7 @@ public class LeonBetParser implements BetParser<LeonOddsDTO> {
             }
         } catch (IOException e) {
             log.error("Не удалось получить страницу. Возможно передан некорректный URL", e);
+            throw new BetParserException("Ошибка ввода/вывода во время загрузки данных со страницы " + url, e);
         }
         StringBuilder object = new StringBuilder();
         String obj = "";
@@ -54,17 +73,20 @@ public class LeonBetParser implements BetParser<LeonOddsDTO> {
         }
         log.info(obj);
         JsonElement element = new JsonParser().parse(obj);
-        JsonArray arr = (JsonArray) ((JsonObject) element).get("initialEvents").getAsJsonObject().get("events");
-        for (int i = 0; i < arr.size(); i++) {
-            JsonElement el = arr.get(i);
-            String eventLink = el.getAsJsonObject().get("url").getAsString();
-            urlsOfEvents.add(eventLink);
-        }
-        return urlsOfEvents;
+        return (JsonArray) ((JsonObject) element).get("initialEvents").getAsJsonObject().get("events");
     }
 
+    /**
+     * Метод возвращает DTO сущность, полученную из веб страницы конкретного евента
+     *
+     * @param url - линк веб страницы евента, который нужно распарсить
+     */
     @Override
-    public List<LeonOddsDTO> loadEventsOfTournament(String url) {
-        return null;
+    public LeonOddsDTO loadEventOdds(String url) {
+        log.info("loadEventOdds {}", url);
+        JsonArray arr = getArrayOfEventsByURL(url);
+        JsonObject object = arr.get(0).getAsJsonObject();
+        Gson gson = new Gson();
+        return gson.fromJson(object, LeonOddsDTO.class);
     }
 }
