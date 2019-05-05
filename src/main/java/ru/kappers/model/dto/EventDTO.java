4018e23c12a34aa9be4fe3cb919179231ff630e1
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-05-05 03:56:40 +0300

    Добавлен класс EventDTO

---
+++ src/main/java/ru/kappers/model/dto/EventDTO.java
@@ -0,0 +1,29 @@
+package ru.kappers.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.kappers.model.utilmodel.Outcomes;
+
+import java.math.BigDecimal;
+
+/**
+ * DTO для Заведенного события
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventDTO {
+    /** Fixture ID */
+    private int f_id;
+    /** возможный исход */
+    private Outcomes outcome;
+    /** коэффициент */
+    private BigDecimal coefficient;
+    /** сколько токенов ставим */
+    private int tokens;
+    /** какую цену назначаем за открытие евента пользователями */
+    private BigDecimal price;
+}
