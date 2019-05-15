6940fca0f3aab58504c4fad6f2754f1df9aae3fd
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-05-15 22:56:57 +0300

    Доработка JPA-сущности для лиги

--- src/main/java/ru/kappers/model/catalog/League.java
+++ src/main/java/ru/kappers/model/catalog/League.java
@@ -5,14 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import ru.kappers.model.dto.leon.SportLeonDTO;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 
+/**
+ * JPA-сущность для лиги
+ */
 @Slf4j
 @Data
 @Builder
@@ -24,16 +28,22 @@ public class League {
     @Id
     @Column(name = "league_id",nullable = false, insertable = false, updatable = false)
     private Integer id;
+    /** название лиги */
     @Column(name="name")
+    @Size(max = 255)
+    @NotBlank
     private String name;
     @Column(name="country")
+    @Size(max = 180)
     private String country;
     @Column(name="season")
+    @Size(max = 9)
     private String season;
     @Column(name="season_start")
     private Timestamp seasonStart;
     @Column(name="season_end")
     private Timestamp seasonEnd;
     @Column(name="logo")
+    @Size(max = 512)
     private String logoUrl;
 }
