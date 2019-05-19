7fc1c313401b9d0439e607f5c560588a8587d0ea
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-05-19 05:55:35 +0300

    Добавлен Spring бин планировщика заданий. Добавлено обновление курсов валют по расписанию (каждый день в 00:00). Добавлен контейнер свойств настройки приложения (в properties-файлах начинается с префикса kappers). Добавлены настройки в properties-файлы. Доработаны unit-тесты.

--- src/main/java/ru/kappers/service/impl/CurrencyServiceImpl.java
+++ src/main/java/ru/kappers/service/impl/CurrencyServiceImpl.java
@@ -2,8 +2,10 @@ package ru.kappers.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kappers.config.KappersProperties;
 import ru.kappers.exceptions.CurrRateGettingException;
 import ru.kappers.model.CurrencyRate;
 import ru.kappers.service.CurrRateService;
@@ -25,11 +27,13 @@ public class CurrencyServiceImpl implements CurrencyService {
 
     private final CurrRateService currRateService;
     private final CBRFDailyCurrencyRatesParser currencyRatesParser;
+    private final KappersProperties kappersProperties;
 
     @Autowired
-    public CurrencyServiceImpl(CurrRateService currRateService, CBRFDailyCurrencyRatesParser currencyRatesParser) {
+    public CurrencyServiceImpl(CurrRateService currRateService, CBRFDailyCurrencyRatesParser currencyRatesParser, KappersProperties kappersProperties) {
         this.currRateService = currRateService;
         this.currencyRatesParser = currencyRatesParser;
+        this.kappersProperties = kappersProperties;
     }
 
     //TODO кстати, данный метод собирались в будущем вызывать по расписанию, это возможно будет настроено как раз в самом методе сервиса или отдельной конфигурации Spring контекста
@@ -46,6 +50,25 @@ public class CurrencyServiceImpl implements CurrencyService {
             throw new CurrRateGettingException("Couldn't get currency rates", ex);
         }
 
+    }
+
+    /**
+     * Обновить курсы валют на сегодня (по расписанию планировщика)
+     */
+    @Scheduled(cron = "${kappers.currency-rates.refresh-cron}")
+    public void refreshCurrencyRatesForTodayByScheduler() {
+        if (kappersProperties.getCurrencyRates().isRefreshCronEnabled()) {
+            log.debug("refreshCurrencyRatesForTodayByScheduler()...");
+        } else {
+            log.debug("disabled call refreshCurrencyRatesForTodayByScheduler()");
+            return;
+        }
+        // метод вызываемый планировщиком не должен выбрасывать исключения, иначе возможно не сработает по расписанию
+        try {
+            refreshCurrencyRatesForToday();
+        } catch (Throwable t) {
+            log.error("refreshCurrencyRatesForTodayByScheduler() error", t);
+        }
     }
 
     public Date getActualCurrencyRateDate(Date date, String fromCurr, String toCurr, boolean currRatesGotToday) {
