b9c3bc5f2d6fc48674f58921c75ae59131640fcc
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-05-21 22:35:47 +0300

    Тестовая доработка отображения Предстоящих событий с ограничением выборки через паджинацию. Тестовый URL запроса: http://localhost:8080/rest/fixture/nextweek?page=1&size=30&sort=eventDate

--- src/main/java/ru/kappers/logic/controller/FixtureController.java
+++ src/main/java/ru/kappers/logic/controller/FixtureController.java
@@ -1,6 +1,8 @@
 package ru.kappers.logic.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -22,15 +24,30 @@ public class FixtureController {
         this.service = service;
     }
 
+/*
     @RequestMapping(value = "/nextweek", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Fixture> getNextWeek() {
         return service.getFixturesNextWeek(Status.NOT_STARTED);
+    }
+*/
+
+// пример тестового URL: http://localhost:8080/rest/fixture/nextweek?page=1&size=30&sort=eventDate
+    @RequestMapping(value = "/nextweek", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<Fixture> getNextWeek(Pageable pageable) {
+        return service.getFixturesNextWeek(Status.NOT_STARTED, pageable);
     }
 
     @RequestMapping(value = "/lastweek", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Fixture> getLastWeek() {
         return service.getFixturesLastWeek(Status.MATCH_FINISHED);
     }
+
+/*
+    @RequestMapping(value = "/lastweek", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<Fixture> getLastWeek(Pageable pageable) {
+        return service.getFixturesLastWeek(Status.MATCH_FINISHED, pageable);
+    }
+*/
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Fixture getFixtureById(@PathVariable int id) {
@@ -42,10 +59,27 @@ public class FixtureController {
         return service.getFixturesToday();
     }
 
+/*
+    @RequestMapping(value = "/today", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<Fixture> getFixturesToday(Pageable pageable) {
+        return service.getFixturesToday(pageable);
+    }
+*/
+
     @RequestMapping(value = "/period", method = RequestMethod.GET)
     public List<Fixture> getFixturesByPeriod(
             @RequestParam("from") @DateTimeFormat(pattern = "yyyy-MM-dd") Date fromDate,
             @RequestParam("to") @DateTimeFormat(pattern = "yyyy-MM-dd") Date toDate) {
         return service.getFixturesByPeriod(new Timestamp(fromDate.getTime()), new Timestamp(toDate.getTime()));
     }
+
+/*
+    @RequestMapping(value = "/period", method = RequestMethod.GET)
+    public Page<Fixture> getFixturesByPeriod(
+            @RequestParam("from") @DateTimeFormat(pattern = "yyyy-MM-dd") Date fromDate,
+            @RequestParam("to") @DateTimeFormat(pattern = "yyyy-MM-dd") Date toDate,
+            Pageable pageable) {
+        return service.getFixturesByPeriod(new Timestamp(fromDate.getTime()), new Timestamp(toDate.getTime()), pageable);
+    }
+*/
 }
