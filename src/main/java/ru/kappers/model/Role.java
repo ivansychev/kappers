d9980deace755a4d41c41acfdcd0083e27fca55e
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-05-23 17:27:29 +0300

    Добавляет константы имен для роли `Role`
    Чтобы исключить дальнейшее дублирование строковых констант в класс `Role` добавляется внутренний класс с константами имен `Role.Names`

--- src/main/java/ru/kappers/model/Role.java
+++ src/main/java/ru/kappers/model/Role.java
@@ -1,8 +1,8 @@
 package ru.kappers.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.collect.ImmutableList;
 import lombok.*;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
@@ -10,6 +10,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * JPA-сущность для роли
+ */
 @Slf4j
 @Data
 @Builder
@@ -33,4 +36,18 @@ public class Role implements Serializable {
     @JsonIgnore
     private List<User> users = new ArrayList<>();
 
+    /**
+     * Класс констант имен ролей. Список всех имен хранится в поле {@link Names#LIST}
+     */
+    public static final class Names {
+        /** Администратор */
+        public static final String ADMIN = "ROLE_ADMIN";
+        /** Пользователь */
+        public static final String USER = "ROLE_USER";
+        /** Kapper (спортивный аналитик) */
+        public static final String KAPPER = "ROLE_KAPPER";
+
+        /** Список всех имен ролей */
+        public static final List<String> LIST = ImmutableList.of(ADMIN, USER, KAPPER);
+    }
 }
