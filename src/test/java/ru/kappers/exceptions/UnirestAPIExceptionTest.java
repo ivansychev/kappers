e3caf3c4b648beec16b2f0f8aa63c34157babfc9
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-06-15 00:52:32 +0300

    UnirestAPIException unit-tests

---
+++ src/test/java/ru/kappers/exceptions/UnirestAPIExceptionTest.java
@@ -0,0 +1,39 @@
+package ru.kappers.exceptions;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class UnirestAPIExceptionTest {
+
+    @Test
+    public void constructorWithMessageOnly() {
+        final String testMessage = "test message";
+
+        UnirestAPIException exception = new UnirestAPIException(testMessage);
+
+        assertThat(exception.getMessage(), is(testMessage));
+    }
+
+    @Test
+    public void constructorWithExceptionOnly() {
+        final Exception testException = new Exception();
+
+        UnirestAPIException exception = new UnirestAPIException(testException);
+
+        assertThat(exception.getMessage(), is(UnirestAPIException.ERROR_TEXT));
+        assertThat(exception.getCause(), is(testException));
+    }
+
+    @Test
+    public void constructorWithMessageAndException() {
+        final String testMessage = "test message2";
+        final Exception testException = new Exception();
+
+        UnirestAPIException exception = new UnirestAPIException(testMessage, testException);
+
+        assertThat(exception.getMessage(), is(testMessage));
+        assertThat(exception.getCause(), is(testException));
+    }
+}
