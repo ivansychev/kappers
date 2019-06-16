7259fc73cf2a0918adb39c6fc4d6e87c6cdd3a15
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-06-16 19:02:20 +0300

    Рефакторинг JPA-сущностей Leon
    Для исключения рекурсий почти во всех сущностях есть исключение связанных сущностей из `equals` и `toString`

--- src/main/java/ru/kappers/model/leonmodels/RunnerLeon.java
+++ src/main/java/ru/kappers/model/leonmodels/RunnerLeon.java
@@ -1,13 +1,11 @@
 package ru.kappers.model.leonmodels;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Slf4j
 @Data
