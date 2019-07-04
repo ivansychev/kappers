19b0d7e4d3aecd74efc75a3f958c3c8ce429c0c0
Ruslan Suleymanov <rusdenis82@gmail.com>
2019-07-04 03:07:03 +0300

    Рефакторинг `EntityMapperController`
    - Переименованы ряд методов и изменены их URL для большей прозрачности
    - Сокращено дублирование кода
    - Сокращено и оптимизировано обращение к БД
    - Изменен стиль кода со Stream API и не только для облегчения чтения кода

--- src/main/java/ru/kappers/logic/controller/EntityMapperController.java
+++ src/main/java/ru/kappers/logic/controller/EntityMapperController.java
@@ -1,6 +1,7 @@
 package ru.kappers.logic.controller;
 
 import com.google.gson.Gson;
+import com.ibm.icu.text.StringTransform;
 import com.ibm.icu.text.Transliterator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,43 +29,50 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping(value = "/rest/mapper/teams")
 public class EntityMapperController {
-    private final TeamService teamService;
-    private final CompetitorLeonService competitorService;
     private static final String CYRILLIC_TO_LATIN = "Latin-Russian/BGN";
-    private final FixtureService fService;
-    private final TeamBridgeService teamBridgeService;
     private static final Gson GSON = new Gson();
 
+    private final TeamService teamService;
+    private final CompetitorLeonService competitorService;
+    private final FixtureService fixtureService;
+    private final TeamBridgeService teamBridgeService;
+    private final StringTransform toLatinTranslit = Transliterator.getInstance(CYRILLIC_TO_LATIN);
+
+
     @Autowired
-    public EntityMapperController(TeamService teamService, CompetitorLeonService competitorService, FixtureService fService, TeamBridgeService teamBridgeService) {
+    public EntityMapperController(TeamService teamService, CompetitorLeonService competitorService, FixtureService fixtureService, TeamBridgeService teamBridgeService) {
         this.teamService = teamService;
         this.competitorService = competitorService;
-        this.fService = fService;
+        this.fixtureService = fixtureService;
         this.teamBridgeService = teamBridgeService;
     }
 
     /**
-     * getRapidTeams возвращает не смапленные сущности Team из базы данных
+     * getUnmappedRapidTeams возвращает не смапленные сущности Team из базы данных
      */
 
     @ResponseBody
-    @RequestMapping(value = "/rapidteams", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Team> getRapidTeams() {
-        log.debug("getRapidTeams()...");
-        List<Integer> mappedIds = teamBridgeService.getAll().stream().map(s -> s.getRapidTeam().getId()).collect(Collectors.toList());
-        return teamService.getAll().stream().filter(s -> !mappedIds.contains(s.getId())).collect(Collectors.toList());
+    @RequestMapping(value = "/unmapped/rapidteams", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Team> getUnmappedRapidTeams() {
+        log.debug("getUnmappedRapidTeams()...");
+        List<Integer> mappedIds = teamBridgeService.getAll().stream()
+                .map(s -> s.getRapidTeam().getId())
+                .collect(Collectors.toList());
+        return teamService.getAllByIdIsNotIn(mappedIds);
     }
 
     /**
-     * getLeonCompetitors возвращает не смапленные сущности CompetitorLeon из базы данных
+     * getUnmappedLeonCompetitors возвращает не смапленные сущности CompetitorLeon из базы данных
      */
 
     @ResponseBody
-    @RequestMapping(value = "/competitors", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CompetitorLeon> getLeonCompetitors() {
-        log.debug("getLeonCompetitors()...");
-        List<Long> mappedIds = teamBridgeService.getAll().stream().map(s -> s.getLeonCompetitor().getId()).collect(Collectors.toList());
-        return competitorService.getAll().stream().filter(s -> !mappedIds.contains(s.getId())).collect(Collectors.toList());
+    @RequestMapping(value = "/unmapped/competitors", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CompetitorLeon> getUnmappedLeonCompetitors() {
+        log.debug("getUnmappedLeonCompetitors()...");
+        List<Long> mappedIds = teamBridgeService.getAll().stream()
+                .map(s -> s.getLeonCompetitor().getId())
+                .collect(Collectors.toList());
+        return competitorService.getAllByIdIsNotIn(mappedIds);
     }
 
     /**
@@ -76,15 +84,21 @@ public class EntityMapperController {
     public Set<Team> completeRapidTeamsFromExistFixtures() {
         log.debug("completeRapidTeamsFromExistFixtures()...");
         Set<Team> teamsFromFixtures = new HashSet<>();
-        List<Fixture> fixtureList = fService.getAll();
+        List<Fixture> fixtureList = fixtureService.getAll();
         for (Fixture f : fixtureList) {
             Team byId = teamService.getById(f.getAwayTeamId());
             if (byId == null) {
-                teamsFromFixtures.add(teamService.save(Team.builder().id(f.getAwayTeamId()).name(f.getAwayTeam()).build()));
+                teamsFromFixtures.add(teamService.save(Team.builder()
+                        .id(f.getAwayTeamId())
+                        .name(f.getAwayTeam())
+                        .build()));
             }
             byId = teamService.getById(f.getHomeTeamId());
             if (byId == null) {
-                teamsFromFixtures.add(teamService.save(Team.builder().id(f.getHomeTeamId()).name(f.getHomeTeam()).build()));
+                teamsFromFixtures.add(teamService.save(Team.builder()
+                        .id(f.getHomeTeamId())
+                        .name(f.getHomeTeam())
+                        .build()));
             }
         }
         return teamsFromFixtures;
@@ -98,22 +112,25 @@ public class EntityMapperController {
     @RequestMapping(value = "/map", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<Team, CompetitorLeon> tryMappingRapidTeamAndLeonCompetitor() {
         log.debug("tryMappingRapidTeamAndLeonCompetitor()...");
-        Map<Team, CompetitorLeon> result = new HashMap<>();
-        Set<String> rapidTeams = teamService.getAll().stream().map(Team::getName).collect(Collectors.toSet());
-        log.debug("tryMappingRapidTeamAndLeonCompetitor(): получено " + rapidTeams.size() + " сущностей Team");
-        List<CompetitorLeon> competitors = competitorService.getAll();
-        List<String> leonComps = competitors.stream().map(CompetitorLeon::getName).collect(Collectors.toList());
-        log.debug("tryMappingRapidTeamAndLeonCompetitor(): получено " + leonComps.size() + " сущностей CompetitorLeon");
-        Transliterator toLatinTrans = Transliterator.getInstance(CYRILLIC_TO_LATIN);
+        final Set<String> rapidTeams = teamService.getAll().stream()
+                .map(Team::getName)
+                .collect(Collectors.toSet());
+        log.debug("tryMappingRapidTeamAndLeonCompetitor(): получено {} сущностей Team", rapidTeams.size());
+        final List<String> leonComps = competitorService.getAll().stream()
+                .map(CompetitorLeon::getName)
+                .collect(Collectors.toList());
+        log.debug("tryMappingRapidTeamAndLeonCompetitor(): получено {} сущностей CompetitorLeon", leonComps.size());
+        final Map<Team, CompetitorLeon> result = new HashMap<>(rapidTeams.size());
+        TeamBridge teamBridge;
         for (String st : rapidTeams) {
             if (leonComps.contains(st)) {
-                result.put(teamService.getByName(st), competitorService.getByName(st));
-                saveTeamBridge(teamService.getByName(st), competitorService.getByName(st));
+                teamBridge = saveTeamBridge(teamService.getByName(st), competitorService.getByName(st));
+                result.put(teamBridge.getRapidTeam(), teamBridge.getLeonCompetitor());
             } else {
-                String res = toLatinTrans.transliterate(st);
+                String res = toLatinTranslit.transform(st);
                 if (leonComps.contains(res)) {
-                    result.put(teamService.getByName(st), competitorService.getByName(res));
-                    saveTeamBridge(teamService.getByName(st), competitorService.getByName(res));
+                    teamBridge = saveTeamBridge(teamService.getByName(st), competitorService.getByName(res));
+                    result.put(teamBridge.getRapidTeam(), teamBridge.getLeonCompetitor());
                 }
             }
         }
@@ -135,15 +152,14 @@ public class EntityMapperController {
         TeamBridgeDTO bridge = GSON.fromJson(content, TeamBridgeDTO.class);
         Team team = teamService.getById(Integer.parseInt(bridge.getRapidTeam()));
         CompetitorLeon competitorLeon = competitorService.getById(Long.parseLong(bridge.getLeonCompetitor()));
-        return teamBridgeService.save(TeamBridge.builder().leonCompetitor(competitorLeon).rapidTeam(team).build());
+        return saveTeamBridge(team, competitorLeon);
     }
 
-    private void saveTeamBridge(Team team, CompetitorLeon competitor) {
-        TeamBridge bridge = TeamBridge.builder()
+    private TeamBridge saveTeamBridge(Team team, CompetitorLeon competitor) {
+        return teamBridgeService.save(TeamBridge.builder()
                 .rapidTeam(team)
                 .leonCompetitor(competitor)
-                .build();
-        teamBridgeService.save(bridge);
+                .build());
     }
 
 
