dbee5466f4ef611b47bb636acd67e3dc4b04babd
ivansychev <mr.ivan.sychev@gmail.com>
2019-07-16 23:18:04 +0300

    Added style transpilation and linting

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -2,6 +2,11 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var devMode = process.env.NODE_ENV !== 'production';
 
+//TODO: add source map to dev
+//TODO: add husky hooks
+//TODO: remove comments in config as soon as wiki for ui appears
+//TODO: add styles support to project
+
 module.exports = {
     mode : devMode ? 'development' : 'production',
     entry: "./src/index.jsx",
@@ -14,7 +19,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 enforce: "pre",
-                loader: "eslint-loader",
+                loader: "eslint-loader", //to lint our code and catch warns and errors before babel transpilation
                 exclude: /node_modules/,
                 options: {
                     emitWarning: true,
@@ -23,7 +28,7 @@ module.exports = {
             },
             {
                 test: /\.(js|jsx)$/,
-                loader: "babel-loader",
+                loader: "babel-loader", //to transpile our modern JS to old JS
                 exclude: /node_modules/,
                 options: {
                     //@TODO move to .babelrc
@@ -40,6 +45,23 @@ module.exports = {
                         ['@babel/plugin-proposal-class-properties', { "loose": true }]
                     ]
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // to link and incorporate our style-in-JS in code
+                    },
+                    {
+                        loader: "css-loader" // CSS --> style-in-JS
+                    },
+                    {
+                        loader: "postcss-loader" // CSS --> to better crossplatform CSS
+                    },
+                    {
+                        loader: "sass-loader" // SCSS --> CSS
+                    }
+                ]
             }
         ]
     },
